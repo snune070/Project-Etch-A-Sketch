a0cd8a69a8cacb9f014defd4ac042ee4f41f81a6
snune070 <snune070@fiu.edu>
2022-11-23 03:57:56 -0500

    Added event listner for 'black' button to set color to black and eventListner to change canvas size to a grid from 1x1 to 100x100

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -14,37 +14,26 @@ for (let i = 0; i < rowNum * colNum; i++) {
     canvas.appendChild(div);
 }
 
+let black = document.getElementById('black');
 
-let size = document.getElementById('size');
-
-size.addEventListener('click', function () {
-    let newSize = parseInt(prompt('Choose a size below 100'));
-    console.log(newSize)
-    rowNum = newSize;
-    colNum = newSize;
-
-
-
-    for (let i = 0; i < rowNum * colNum; i++) {
-        let newDiv = document.createElement('div')
-        newDiv.style.height = '1fr';
-        newDiv.style.width = '1fr';
-        newDiv.style.padding = '0px';
-        newDiv.style.margin = '0px';
-        newDiv.classList.add('paint')
-        canvas.appendChild(newDiv);
+black.addEventListener('click', function () {
+    const defaultColor = document.querySelectorAll('.paint');
+    for (let defaultColorElement of defaultColor) {
+        defaultColorElement.addEventListener('mouseover', function () {
+            defaultColorElement.style.backgroundColor = 'black';
+        })
     }
-
 })
+
 
 let random = document.getElementById('random');
 
-random.addEventListener('click', function (){
+random.addEventListener('click', function () {
     let randomColors = Math.floor(Math.random() * 16777215).toString(16);
 
     const randomColor = document.querySelectorAll('.paint')
     for (let randomColorElement of randomColor) {
-        randomColorElement.addEventListener('mouseover', function (){
+        randomColorElement.addEventListener('mouseover', function () {
             randomColorElement.style.backgroundColor = "#" + randomColors;
         })
     }
@@ -54,13 +43,43 @@ random.addEventListener('click', function (){
 
 let clear = document.getElementById('clear');
 
-clear.addEventListener('click', function (){
+clear.addEventListener('click', function () {
     let elements = document.getElementsByClassName('paint')
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor = '';
     }
 })
 
+
+let chooseSize = document.getElementById('size');
+
+chooseSize.addEventListener('click', function () {
+    let newSize = prompt('Choose a size below 100');
+    console.log(newSize)
+    rowNum = parseInt(newSize);
+    colNum = parseInt(newSize);
+
+   const newPlane = document.getElementById('container');
+   while (newPlane.hasChildNodes()){
+       newPlane.removeChild(newPlane.firstChild)
+   }
+
+   newPlane.style.gridTemplateColumns = `repeat(${colNum} , 1fr)`;
+   newPlane.style.gridTemplateRows = `repeat(${rowNum}, 1fr)`;
+
+    for (let i = 0; i < rowNum * colNum; i++) {
+        let newDiv = document.createElement('div')
+        newDiv.style.height = 'auto';
+        newDiv.style.width = 'auto';
+        newDiv.style.padding = '0px';
+        newDiv.style.margin = '0px';
+        newDiv.classList.add('paint')
+        canvas.appendChild(newDiv);
+    }
+
+    setDefault();
+
+})
 
 const paint = document.querySelectorAll('.paint');
 for (let paintElement of paint) {
@@ -71,4 +90,13 @@ for (let paintElement of paint) {
     })
 }
 
+function setDefault(){
+    const newPaint = document.querySelectorAll('.paint')
+
+    for (let newPaintElement of newPaint) {
+        newPaintElement.addEventListener('mouseover', function (){
+            newPaintElement.style.backgroundColor = 'black';
+        })
+    }
+}
 
